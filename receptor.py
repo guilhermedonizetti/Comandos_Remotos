ff5f17719bd5013000483e11be3cd3f5f1310074
guilhermedonizetti <guilhermetecnologias@gmail.com>
2022-02-14 21:06:11 -0300

    Correcao.

--- receptor.py
+++ receptor.py
@@ -1,12 +1,12 @@
 import paho.mqtt.subscribe as subscribe
 import funcoes as fc
 from webbrowser import open
-from os import execl, sync
+from os import execl, system
 
 x = True
 while x == True:
 
-    msg = subscribe.simple("remotoTeste/Gui", hostname="test.mosquitto.org", port=1883)
+    msg = subscribe.simple(["remotoTeste/Gui", "remotoTeste/Gui/Desligar"], hostname="test.mosquitto.org", port=1883)
     comando = msg.payload.decode("utf8")
 
     # Verifica se foi informado um link para abrir no navegador
@@ -17,8 +17,11 @@ while x == True:
     # Verifica se foi informado um programa para ser iniciado
     status, caminho = fc.verificarPrograma(comando)
     if status:
-        sync()
         execl(caminho, "code")
+
+    # Verifica se deve desligar o computador
+    if comando == "Desligar":
+        system("shutdown /s /t 1")
     
     # Verifica se deve encerrar o programa
     try:
